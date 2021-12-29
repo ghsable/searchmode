5f1ff544c32bc6061f5ef1c7d0b53bfa19ceef8f
ghsable <sn.sable005@gmail.com>
2021-12-29 22:05:47 +0900

    Add Brave

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const json = {
     "Yahoo!": "https://search.yahoo.co.jp/search?p=",
     "Bing": "https://www.bing.com/search?q=",
     "Wikipedia": "https://ja.wikipedia.org/wiki/",
+    "Brave": "https://search.brave.com/search?q=",
     "DuckDuckGo": "https://duckduckgo.com/?q=",
     "Google": "https://www.google.com/search?q=",
     "Ecosia": "https://www.ecosia.org/search?q=",
